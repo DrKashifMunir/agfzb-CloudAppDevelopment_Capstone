c54b89cff7e3579ef02fd5e9c7f76d156abe9220
DrKashifMunir <kashif.munir@nu.edu.pk>
2024-01-06 20:38:48 -0500

    Adding temporary changes to Github

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -77,10 +77,22 @@ WSGI_APPLICATION = 'djangobackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+#Postgres
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'MjQ4MTUta2FzaGlm',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
